5449cd7da58887caba5385055252dd30d71b37d8
NAoHR <najmim625@gmail.com>
2022-05-08 01:35:09 +0700

    update: add unfollow and follow feature on interact route

--- controller/interactController.js
+++ controller/interactController.js
@@ -1,4 +1,5 @@
 const PostModel = require("../model/Post.model");
+const UserModel = require("../model/User.model");
 const {errorHandler} = require("../utils/errorHandling");
 
 exports.likePost = async (req,res) => {
@@ -55,6 +56,92 @@ exports.unlikePost = async (req,res) => {
             message : "data not found"
         })
 
+    }catch(e){
+        const errorState = errorHandler(e);
+        return res.status(errorState.code).json(errorState.errorData);
+    }
+}
+
+exports.followUser = async (req,res) => {
+    try{
+        const {uid} = req.uid;
+        const {userId} = req.params;
+
+        if(uid != userId){
+            const requestedUser = await UserModel.findById(userId);
+
+            if(requestedUser){
+                const currentUser = await UserModel.findById(uid);
+
+                if(requestedUser.followers.indexOf(uid) === -1){
+                    requestedUser.followers.push(uid);
+                    currentUser.followings.push(userId);
+
+                    await requestedUser.save();
+                    await currentUser.save();
+
+                    return res.status(200).json({
+                        ok : true,
+                        message : "followed"
+                    })
+                }
+                return res.status(403).json({
+                    ok : true,
+                    message : "already followed"
+                })
+            }
+            return res.status(401).json({
+                ok : false,
+                message : "data not found"
+            })
+        }
+        return res.status(403).json({
+            ok : true,
+            message : "cant follow yourself"
+        })
+    }catch(e){
+        const errorState = errorHandler(e);
+        return res.status(errorState.code).json(errorState.errorData);
+    }
+}
+
+exports.unfollowUser = async (req,res) => {
+    try{
+        const {uid} = req.uid;
+        const {userId} = req.params;
+
+        if(uid != userId){
+            const requestedUser = await UserModel.findById(userId);
+
+            if(requestedUser){
+                const currentUser = await UserModel.findById(uid);
+
+                if(requestedUser.followers.indexOf(uid) >= 0){
+                    requestedUser.followers = requestedUser.followers.filter(item => item != uid);
+                    currentUser.followings = requestedUser.followings.filter(item => item != userId);
+
+                    await requestedUser.save();
+                    await currentUser.save();
+
+                    return res.status(200).json({
+                        ok : true,
+                        message : "unfollowed"
+                    })
+                }
+                return res.status(403).json({
+                    ok : true,
+                    message : "already unfollowed"
+                })
+            }
+            return res.status(401).json({
+                ok : false,
+                message : "data not found"
+            })
+        }
+        return res.status(403).json({
+            ok : true,
+            message : "cant unfollow yourself"
+        })
     }catch(e){
         const errorState = errorHandler(e);
         return res.status(errorState.code).json(errorState.errorData);
--- router/interactRouter.js
+++ router/interactRouter.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const interactController = require("../controller/interactController")
 
-router.put("/like/:postId", authMiddleware.authenticated,interactController.likePost);
-router.put("/unlike/:postId", authMiddleware.authenticated,interactController.unlikePost)
+router.put("/like/:postId", authMiddleware.authenticated, interactController.likePost);
+router.put("/unlike/:postId", authMiddleware.authenticated, interactController.unlikePost);
+router.put("/follow/:userId", authMiddleware.authenticated, interactController.followUser);
+router.put("/unfollow/:userId", authMiddleware.authenticated, interactController.unfollowUser);
 
 
 module.exports = router;
