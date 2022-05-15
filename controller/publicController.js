17d61a17d8fa290ea4ebe9316c38ad76514e2091
NAoHR <najmim625@gmail.com>
2022-05-15 23:08:42 +0700

    update: add search user route

--- controller/publicController.js
+++ controller/publicController.js
@@ -1,4 +1,6 @@
+const res = require("express/lib/response");
 const CommentModel = require("../model/Comment.model");
+const DetailModel = require("../model/Detail.model");
 const PostModel = require("../model/Post.model");
 const UserModel = require("../model/User.model");
 const {errorHandler,publicUserParser,publicPostParser} = require("../utils/utils");
@@ -39,6 +41,46 @@ exports.getPost = async (req,res) => {
             message : "data not found",
             data : []
         })
+    }catch(e){
+        const errorState = errorHandler(e);
+        return res.status(errorState.code).json(errorState.errorData);
+    }
+}
+
+exports.getUser = async (req,res) => {
+    try{
+        const {username} = req.params;
+        const user = await UserModel.findOne({username : username});
+
+        if(user){
+            const userDetail = await DetailModel.findById(user.detail);
+            const userPosts = await PostModel.find({_id : {$in : user.post}})
+            const userFollowings = await UserModel.find({_id : {$in : user.followings.slice(0,5)}});
+            const userFollowers = await UserModel.find({_id : {$in : user.followers.slice(0,5)}})
+
+            return res.status(200).json({
+                ok : true,
+                message : "data fetched",
+                data : {
+                    _id : user._id,
+                    username : user.username,
+                    fullname : user.fullname,
+                    email : user.email,
+                    detail : userDetail,
+                    posts : userPosts,
+                    followers : userFollowers.map((v)=>{
+                        return publicUserParser(v)
+                    }),
+                    followings : userFollowings.map((v)=>{
+                        return publicUserParser(v)
+                    }),
+                }
+            })
+        }
+        return res.status(401).json({
+            ok : true,
+            message : "data not found"
+        });
     }catch(e){
         const errorState = errorHandler(e);
         return res.status(errorState.code).json(errorState.errorData);
--- router/publicRouter.js
+++ router/publicRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const publicController = require("../controller/publicController");
 
 router.get("/post/:postId", publicController.getPost);
+router.get("/user/:username",publicController.getUser);
 
 module.exports = router;
